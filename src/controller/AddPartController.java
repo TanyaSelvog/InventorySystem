472084d72561dc843cf27acdcbde420c2778fb61
Tanya Selvog <tanyaselvog@gmail.com>
2021-10-06 13:07:08 -0700

    kept getPart method in AddPartController

--- src/controller/AddPartController.java
+++ src/controller/AddPartController.java
@@ -70,50 +70,131 @@ public class AddPartController implements Initializable {
     }
 
 
-    public void onSaveBtnAddPart(ActionEvent actionEvent) throws IOException {
+        private Part getPart () {
+            boolean isValid = true;
+            List<String> messages = new ArrayList<>();
+            Double price = null;
+            Integer stock = null;
+            Integer max = null;
+            Integer min = null;
+            Integer machineID = null;
+            String companyName = null;
 
 
-        String name = nameTextField.getText().trim();
-        if (name.isEmpty()) {
-            Alert alert = new Alert(Alert.AlertType.ERROR, "Data is missing in name field");
-            alert.showAndWait();
-        } else {
-            try {
-                int stock = Integer.parseInt(stockTextField.getText());
-                int max = Integer.parseInt(maxTextField.getText().trim());
-                int min = Integer.parseInt(minTextField.getText().trim());
-                if (min >= max) {
-                    Alert alert = new Alert(Alert.AlertType.ERROR, "Min value needs to be less than Max value");
-                }
-                // int machineID = Integer.parseInt(machineIDField.getText().trim());
-                double price = Double.parseDouble(costPartText.getText().trim());
+            String name = nameTextField.getText();
+            if (name.isEmpty()) {
+                isValid = false;
+                messages.add("No data in Name field");
+            }
 
-            } catch (NumberFormatException nfe) {
-                Alert alert = new Alert(Alert.AlertType.ERROR, "Data is missing in a field");
-                alert.showAndWait();
+            String priceText = costPartText.getText();
+            if (priceText.isEmpty()) {
+                isValid = false;
+                messages.add("No data in Price field");
+            } else try {
+                price = Double.parseDouble(priceText);
+            } catch (NumberFormatException e) {
+                isValid = false;
+                messages.add("Price is not a double.");
             }
 
 
-            //   Part part = getPart();
+            String stockText = stockTextField.getText();
+            if (stockText.isEmpty()) {
+                isValid = false;
+                messages.add("No data in Stock field");
+            } else try {
+                stock = Integer.parseInt(stockText);
+            } catch (NumberFormatException e) {
+                isValid = false;
+                messages.add("Stock is not an integer");
+            }
+
+            String minText = minTextField.getText();
+            if (minText.isEmpty()) {
+                isValid = false;
+                messages.add("No data in Min field");
+            } else try {
+                min = Integer.parseInt(minText);
+            } catch (NumberFormatException e) {
+                isValid = false;
+                messages.add("Min is not an integer");
+            }
+
+            String maxText = maxTextField.getText();
+            if (maxText.isEmpty()) {
+                isValid = false;
+                messages.add("No data in Max field");
+            } else try {
+                max = Integer.parseInt(maxText);
+            } catch (NumberFormatException e) {
+                isValid = false;
+                messages.add("Max is not an integer");
+            }
+
+            if (min != null && max != null && min >= max) {
+                isValid = false;
+                messages.add("Min value needs to be less than Max value");
+            }
+
+            if (tgroup.getSelectedToggle() == null) {
+                isValid = false;
+                messages.add("Select either In-House or Outsourced");
+            }
+
+            if (inHouseRadio.isSelected()) {
+                String machineText = partDiffLabel.getText();
+                if (machineText.isEmpty()) {
+                    isValid = false;
+                    messages.add("No data in MachineID field");
+                } else try {
+                    machineID = Integer.parseInt(machineText);
+                } catch (NumberFormatException e) {
+                    isValid = false;
+                    messages.add("MachineID is not an integer");
+                }
+
+
+            }
+
+            if (outsourcedBtn.isSelected()) {
+                String company = partDiffLabel.getText();
+                if (company.isEmpty()) {
+                    isValid = false;
+                    messages.add("No data in Company Name field");
+                }
+
+            }
+
+            if (isValid) {
+
+                Integer id = Inventory.addID();
+                if (inHouseRadio.isSelected()) {
+                    InHouse newPartInHouse = new InHouse(id, name, price, stock, min, max, machineID);
+                    Inventory.addPart(newPartInHouse);
+                    return newPartInHouse;
+
+                } else if (outsourcedBtn.isSelected()) {
+                    Outsourced newOutsourced = new Outsourced(id, name, price, stock, min, max, companyName);
+                    Inventory.addPart(newOutsourced);
+                    return newOutsourced;
+                }
+
+            }
+            Alert alert = new Alert(Alert.AlertType.ERROR, String.join("\n", messages));
+            alert.showAndWait();
+            return null;
+
+
+
+        }
+
+        public void onSaveBtnAddPart(ActionEvent actionEvent) throws IOException {
+            Part part = getPart();
             returnMainScreen(actionEvent);
 
-
         }
-
-    }
-    /**    private void minMaxCompare(){
-            if (min != null && max != null && min >= max) {
-                Alert alert = new Alert(Alert.AlertType.ERROR, "Min is more than max");
-                alert.showAndWait();
-            }
-
-        }
+}
 
 
-        if (tgroup.getSelectedToggle() == null) {
-        isValid = false;
-        messages.add("Select either In-House or Outsourced");
-    }
-}*/
-    }
 
